e5b0461ba8613aeb5a277dc998fe4ed2130bc315
yao-zheng-yi <1079543838@qq.com>
2018-10-10 14:52:02 +0800

    fix: fix LinesCount count the line of number

--- Java/031602206&031602209/src/LinesCount.java
+++ Java/031602206&031602209/src/LinesCount.java
@@ -9,20 +9,23 @@ public class LinesCount {
     * @return the sum of lines
     */
     public int linesNumber(String filePath) {
-        int sum = 0;
+        int sum_a = 0;
+        int sum_n = 0;
         try {
             FileReader fileReader = new FileReader(filePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                if (line.length() != 0 && !line.matches("\\s+")) {
-                    sum++;
+                if (line.matches("\\d+")) {
+                    sum_n++;
+                } else if (line.length() != 0 && !line.matches("\\s+")) {
+                    sum_a++;
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return sum;
+        return sum_a;
     }
 
 }
